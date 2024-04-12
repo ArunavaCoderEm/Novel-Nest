ee1ffa83d5cac8b86ea9982b89414e2729cc55e7
ArunavaCoderEm <acecankill@gmail.com>
2024-04-12 14:17:43 +0530

    a lot of pushes pages and initial designs done

--- src/Pages/Page.jsx
+++ src/Pages/Page.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Home from './Home'
 
 export default function Page() {
   return (
     <>
     <Navbar/>
+    <Home/>
     </>
   )
 }
