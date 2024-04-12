fcabbf2fe6f6fe9c5dfaf3990a725b7a10584fc4
ArunavaCoderEm <acecankill@gmail.com>
2024-04-12 15:13:35 +0530

    push

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <section className="text-gray-600 body-font bgh">
+    <section className="text-gray-600 body-font bgh bg-[url('./bg.jpg')]">
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-gray-900">Books are a uniquely 
