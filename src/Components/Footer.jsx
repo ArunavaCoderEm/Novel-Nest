a8f360272bca376ce7f66ae3c36db86a162bd538
ArunavaCoderEm <acecankill@gmail.com>
2024-08-13 21:40:34 +0530

    changes portf

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <footer className='w-full mt-5 bg-amber-900 justify-center items-center  bottom-0'>
-        <h2 className='text-white text-center p-1 font-mono font-xl'>Made By <Link to="https://godard.vercel.app/" target='_blank' className='text-amber-200'>@Godard</Link> with vite + React, Tailwind & 💜</h2>
+        <h2 className='text-white text-center p-1 font-mono font-xl'>Made By <Link to="https://meard.vercel.app/" target='_blank' className='text-amber-200'>@Godard</Link> with vite + React, Tailwind & 💜</h2>
     </footer>
   )
 }
